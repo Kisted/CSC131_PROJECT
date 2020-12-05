2c4e9651ce8763ff45f4fb8d541d469ac3392cc2
Kisted <kbisted@gmail.com>
2020-12-04 16:14:20 -0800

    Added Eclipse Project for API created intiial design doc

---
+++ MovieAPI/src/servlets/MovieServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+public class MovieServlet extends HttpServlet {
+
+	@Override
+	public void doGe(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		
+		String reqURL = req.getRequestURI();
+		String
+		
+	}
+	
+}
